d0137f48faf275ca293e073591d3de4ed207ec68
Tuan Pham <tuanpham@chidoanh.com>
2023-03-17 13:55:46 +0700

    Add new phaser

---
+++ assets/js/Scene1.js
@@ -0,0 +1,14 @@
+class Scene1 extends Phaser.Scene {
+    constructor() {
+        super("bootGame");
+    }
+
+    preload() {
+        this.load.image("background", "assets/images/background.png");
+    }
+
+    create() {
+        this.add.text(20, 20, "Loading game...");
+        this.scene.start("playGame");
+    }
+}
