c7e43d350f443423584d54d4dee324dda0b300b8
Tuan Pham <tuanpham@chidoanh.com>
2023-03-21 09:46:05 +0700

    Moving image

--- assets/js/Scene1.js
+++ assets/js/Scene1.js
@@ -5,6 +5,9 @@ class Scene1 extends Phaser.Scene {
 
     preload() {
         this.load.image("background", "assets/images/background.png");
+        this.load.image("ship", "assets/images/ship.png");
+        this.load.image("ship2", "assets/images/ship2.png");
+        this.load.image("ship3", "assets/images/ship3.png");
     }
 
     create() {
