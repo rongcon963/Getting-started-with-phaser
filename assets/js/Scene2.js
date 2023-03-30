46c67ae086925d27fdeba8b47a081464b3120642
Tuan Pham <tuanpham@chidoanh.com>
2023-03-30 11:45:57 +0700

    Add physic power Up

--- assets/js/Scene2.js
+++ assets/js/Scene2.js
@@ -59,6 +59,46 @@ class Scene2 extends Phaser.Scene {
             font: "25px Arial",
             fill: "yellow"
         });
+
+        this.anims.create({
+            key: "red",
+            frames: this.anims.generateFrameNumbers("power-up", {
+                start: 0,
+                end: 1
+            }),
+            frameRate: 20,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "gray",
+            frames: this.anims.generateFrameNumbers("power-up", {
+                start: 2,
+                end: 3
+            }),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.physics.world.setBoundsCollision();
+
+        this.powerUps = this.physics.add.group();
+
+        var maxObjects = 4;
+        for (var i = 0; i <= maxObjects; i++) {
+            var powerUp = this.physics.add.sprite(16, 16, "power-up");
+            this.powerUps.add(powerUp);
+            powerUp.setRandomPosition(0, 0, game.config.width, game.config.height);
+
+            if (Math.random() > 0.5) {
+                powerUp.play("red");
+            } else {
+                powerUp.play("gray");
+            }
+    
+            powerUp.setVelocity(100, 100);
+            powerUp.setCollideWorldBounds(true);
+            powerUp.setBounce(1);
+        }
     }
 
     moveShip(ship, speed) {
--- assets/js/game.js
+++ assets/js/game.js
@@ -4,5 +4,11 @@ let config = {
     backgroundColor: 0x000000,
     scene: [Scene1, Scene2],
     pixelArt: true,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: false
+        }
+    }
 }
 let game = new Phaser.Game(config);
